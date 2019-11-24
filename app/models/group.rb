0df016e8e77d4bf8ae0d4d35c11a8b0e29887225
yusuketono <yusuke.tono@di-v.co.jp>
2019-11-24 21:23:59 +0900

    Revert

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,17 @@
 class Group < ApplicationRecord
   has_many :group_users
+  has_many :messages
   has_many :users, through: :group_users
-  validates :name, presence: true, uniqueness: true
+
+  validates :name, presence: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      # 最新のメッセージを変数last_messageに代入しつつ、メッセージが投稿されているかどうかで場合分けを行なっています。
+      last_message.content? ? last_message.content : '画像が投稿されています'
+      # 条件式 ? trueの時の値 : falseの時の値
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
