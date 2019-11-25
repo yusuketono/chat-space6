97fb1d03b3aa33f25617247e8ff89566e06d6272
yusuketono <yusuke.tono@di-v.co.jp>
2019-11-25 10:45:55 +0900

    event

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,2 +1,8 @@
 $(function(){
+// $(function(){処理});でwindow.addEventListener('load', function() { 処理 });(HTMLの読み込み)をしている
+  $('.form').on('submit',function(e){
+    e.preventDefault()
+    console.log('メッセージが送信されました');
+  })
 });
+
